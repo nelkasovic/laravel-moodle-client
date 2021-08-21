e6b450fb0ae9e61dc07e24cbfe6850ea08a02393
Nermin Elkasovic <nermin.elkas@gmail.com>
2021-08-21 10:51:29 +0200

    Add getAll() to CourseService Facade

--- src/Facades/CourseService.php
+++ src/Facades/CourseService.php
@@ -3,8 +3,12 @@
 namespace Wimando\LaravelMoodle\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use Wimando\LaravelMoodle\Entities\CourseCollection;
 use Wimando\LaravelMoodle\Services\Course;
 
+/**
+ * @method static CourseCollection getAll()
+ */
 class CourseService extends Facade
 {
     protected static function getFacadeAccessor(): string

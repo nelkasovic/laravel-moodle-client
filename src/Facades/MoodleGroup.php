a345ca11c9ab7972031999bcc4901a098a2e6613
Nermin Elkasovic <nermin.elkas@gmail.com>
2021-08-23 13:11:36 +0200

    Group service and resource fix

--- src/Facades/MoodleGroup.php
+++ src/Facades/MoodleGroup.php
@@ -3,11 +3,14 @@
 namespace Wimando\LaravelMoodle\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use Wimando\LaravelMoodle\Clients\ClientAdapterInterface;
 use Wimando\LaravelMoodle\Resources\GroupResource;
 use Wimando\LaravelMoodle\Resources\GroupResourceCollection;
 use Wimando\LaravelMoodle\Resources\UserResourceCollection;
+use Wimando\LaravelMoodle\Services\GroupService;
 
 /**
+ * @method static GroupService setClient(ClientAdapterInterface $client)
  * @method static GroupResource getById(int $moodleGroupId)
  * @method static GroupResourceCollection getCourseGroups(int $moodleCourseId)
  * @method static UserResourceCollection getGroupMembers(int $moodleGroupId)
